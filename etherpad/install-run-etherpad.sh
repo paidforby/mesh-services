35f487d61ef822148dafa24280aeeb8461928c68
paidforby <ggallo102@gmail.com>
2018-07-12 13:59:57 -0700

    now should work for testing

--- etherpad/install-run-etherpad.sh
+++ etherpad/install-run-etherpad.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-$ETHERPAD_HOME = ~/local/etherpad
-
 # Create bash profile and install NodeJS with NVM
 touch ~/.bashrc
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-source ~/.bashrc
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 7.10
 
 # Clone, install, and run etherpad-lite
-mkdir -p $ETHERPAD_HOME
-cd $ETHERPAD_HOME
-git clone git://github.com/ether/etherpad-lite.git 
-cd etherpad-lite
+mkdir -p ~/local/etherpad
+git clone git://github.com/ether/etherpad-lite.git ~/local/etherpad
+cd ~/local/etherpad/
 bin/run.sh 
 
